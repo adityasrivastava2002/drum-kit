7dbdf509cdad09bbe95a8279cbc1bb6f1875203a
Aditya <adityasrivastava2002@gmail.com>
2022-10-10 19:24:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        keySound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+document.addEventListener("keypress", function (event) {
+    keySound(event.key);
+    buttonAnimation(event.key);
+});
+function keySound(key) {
+    switch (key) {
+        case 'j':
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case 'k':
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        case 'l':
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            console.log(this.key);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var active=document.querySelector("."+currentKey);
+    active.classList.add("pressed");
+    setTimeout(function(){
+        active.classList.remove("pressed"),1000
+    });
+}
